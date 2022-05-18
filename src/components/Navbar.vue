8f39570e894c707da0598ddb8e702d90f0baa32e
satrio92 <kukuhsatrio920@gmail.com>
2022-05-18 19:28:21 +0700

    delete struktur and proker

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,8 +7,6 @@
         <ul class="text-lg font-medium flex gap-8 items-center">
             <li><router-link to="/" class="flex gap-2"><i class="bi bi-house"></i>Home</router-link></li>
             <li><router-link to="/about" class="flex gap-2"><i class="bi bi-people"></i>Tentang Kami</router-link></li>
-            <li><router-link to="/about" class="flex gap-2"><i class="bi bi-diagram-2"></i>Struktur Bidang 1</router-link></li>
-            <li><router-link to="/about" class="flex gap-2"><i class="bi bi-menu-up"></i>Program Kerja</router-link></li>
             <li class="flex gap-4 items-center"><input type="text" class="border-2 border-white bg-transparent outline-none rounded-md px-4 py-1" v-if="search"><i class="bi bi-search cursor-pointer" @click="search = !search"></i></li>
         </ul>
     </div>
